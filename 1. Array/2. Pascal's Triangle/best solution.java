c3312610cf8239bfb6e1e1610227ad89e62fc70e
Ravi Anand <ravianandfbg@gmail.com>
2022-07-02 21:38:03 +0530

    code

--- 1. Array/2. Pascal's Triangle/best solution.java
+++ 1. Array/2. Pascal's Triangle/best solution.java
@@ -1,24 +1,27 @@
 // T.C : O(N) 
 // S.C : O(1)
 
-import java.util.*;
 class Solution {
-      public List<List<Integer>> generate(int numRows) {
-          List<List<Integer>> res = new ArrayList<List<Integer>>();
-          List<Integer> row , prev_row = null;
-          
-          for(int i = 0; i < numRows ; i++){
-              row = new ArrayList<Integer>();
-              for(int j = 0 ; j <= i ; j++){
-                  if(j == 0 || j == i){
-                      row.add(1);
-                  }
-                  else
-                      row.add(prev_row.get(j - 1) + prev_row.get(j));
-              }
-                      prev_row = row;
-                      res.add(row); 
-          }
-          return res;
-      }
-  }
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        
+        List<Integer> curRow , prevRow = null;
+        
+        for(int i = 0 ; i < numRows ; i++){
+            curRow = new ArrayList<>();
+            
+            for(int j = 0 ; j <= i ; j++){
+                if(j == 0 || j == i){
+                    curRow.add(1);
+                }
+                else{
+                    curRow.add(prevRow.get(j - 1) + prevRow.get(j));   
+                }
+            }
+            prevRow = curRow;
+            res.add(curRow);
+        }
+        
+        return res;
+    }
+}
