ca958640da76bd0a1165fed62e5660bbedf0c1ea
Ravi Anand <ravianandfbg@gmail.com>
2022-07-17 21:12:01 +0530

    code

--- 1. Array/7. Rotate Image/Rotate Image.java
+++ 1. Array/7. Rotate Image/Rotate Image.java
@@ -1,19 +1,30 @@
+// time : O(N^2) + O(N^2)  first O(N^2) is for transpose  , second O(N^2) is for reverse
+// Overall time : 2 * O(N^2) = O(N^2)
+// space : O(1)
+
 class Solution {
-      public void rotate(int[][] matrix) {
-          for(int i = 0 ; i < matrix.length ; i++){
-              for(int j = i ; j < matrix[0].length ; j++){
-                  int temp = matrix[i][j];
-                  matrix[i][j] = matrix[j][i];
-                  matrix[j][i] = temp;
-              }
-          }
-          
-          for(int i = 0 ; i < matrix.length ; i++){
-              for(int j = 0 ; j < matrix.length / 2 ; j++){
-                  int temp = matrix[i][j];
-                  matrix[i][j] = matrix[i][matrix.length-1-j];
-                  matrix[i][matrix.length-1-j] = temp;
-              }
-          }
-      }
-  }
+    public void rotate(int[][] matrix) {
+        // transpose
+        for(int i = 0 ; i < matrix.length ; i++){
+            for(int j = i ; j < matrix[0].length ; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        
+        for(int i = 0 ; i < matrix.length ; i++){
+            int li = 0;
+            int ri = matrix.length - 1;
+            
+            while(li < ri){
+                int temp = matrix[i][li];
+                matrix[i][li] = matrix[i][ri];
+                matrix[i][ri] = temp;
+                
+                li++;
+                ri--;
+            }
+        }
+    }
+}
