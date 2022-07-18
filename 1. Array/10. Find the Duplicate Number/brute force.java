48f8d7eab72db5200b5b93d5cad2135331609576
Ravi Anand <ravianandfbg@gmail.com>
2022-07-18 12:48:36 +0530

    code

---
+++ 1. Array/10. Find the Duplicate Number/brute force.java
@@ -0,0 +1,16 @@
+// time : O(NlogN + N)
+// space : O(1)
+
+class Solution {
+      public int findDuplicate(int[] nums) {
+          Arrays.sort(nums);
+          
+          for(int i = 0 ; i < nums.length-1 ; i++){
+              if(nums[i] == nums[i + 1]){
+                  return nums[i];
+              }
+          }
+          
+          return 0;
+      }
+  }
