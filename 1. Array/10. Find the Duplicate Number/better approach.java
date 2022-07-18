9292940d6c7e726123ce0e6a2d4b0593dbe331eb
Ravi Anand <ravianandfbg@gmail.com>
2022-07-18 12:54:39 +0530

    code

---
+++ 1. Array/10. Find the Duplicate Number/better approach.java
@@ -0,0 +1,20 @@
+// time : O(N)
+// space : O(N)
+
+class Solution {
+      public int findDuplicate(int[] nums) {
+          int n = nums.length;
+          int[] freq = new int[n + 1];
+          
+          for(int i = 0 ; i < n ; i++){
+              if(freq[nums[i]] == 0){
+                  freq[nums[i]] += 1;
+              }
+              else{
+                  return nums[i];
+              }
+          }
+          
+          return 0;
+      }
+  }
