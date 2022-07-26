bb204feeb2e395601a449bf18162e88cb61bc7a6
Ravi Anand <ravianandfbg@gmail.com>
2022-07-26 22:31:33 +0530

    code

---
+++ 1. Array/14. Pow(x, n)/brute force.java
@@ -0,0 +1,14 @@
+//time : O(N)
+// Space : O(1)
+
+class Solution {
+      public double myPow(double x, int n) {
+          
+          double ans = 1.0;
+          for(int i = 0 ; i < n ; i++){
+              ans = ans * x;
+          }
+          
+          return ans;
+      }
+  }
